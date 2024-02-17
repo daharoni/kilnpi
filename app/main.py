920a6f83761bde849c513dfc2f6675c8e076639b
Daniel Aharoni <dbaharoni@gmail.com>
2024-02-16 18:47:01 -0800

    Add if __main__ statement to be able to run with uvicorn

--- app/main.py
+++ app/main.py
@@ -18,3 +18,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # assuming you have a function in your database module for it:
 from database import init_db
 init_db()
+
+
+# Run the application directly using uvicorn when this file is executed as the main program
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
